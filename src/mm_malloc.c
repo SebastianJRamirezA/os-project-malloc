130cde4d71c90aae801d37549ac9f8416d2c04f8
Sebastian Ramirez <sebastian.jose.ramirez.a@gmail.com>
2026-03-12 16:47:45 -0400

    feat: Implementación inicial de First-Fit dentro de malloc

--- src/mm_malloc.c
+++ src/mm_malloc.c
@@ -3,33 +3,42 @@
 
 // Inicio de la lista enlazada del heap
 void *base = NULL;
+int a = 0;
 
 void *my_malloc(size_t size) {
     // TODO: Implementar First-Fit o Best-Fit
     // 1. Verificar si hay un bloque libre del tamaño adecuado.
     // 2. Si no, pedir espacio al OS con sbrk().
-    
-    // Si aun no se ha solicitado memoria en el proceso
-    if (base == NULL)
-    {        
-        // Ubicar inicio del nuevo bloque de memoria y solicitarlo al sistema operativo
-        void *start_ptr = sbrk(0);
-        if (sbrk(size + META_SIZE) == (void *)-1)
+
+    block_meta *current = (block_meta *)base;
+    while (current != NULL)
+    {
+        if (current->free && (current->size >= size))
         {
-            perror("Error al asignar memoria");
-            return NULL;
+            // Encontramos un bloque libre adecuado
+            current->free = 0; // Marcar como ocupado
+            return (void *)(current + META_SIZE); // Retornar el espacio después de la metadata
         }
-
-        // Apuntador al inicio del bloque a retornar
-        void *ptr = start_ptr + META_SIZE;
-
-        // Crear metadata del bloque y almacenarla al inicio del bloque asignado
-        block_meta meta = {size, NULL, 0, 42};
-        *(block_meta *)start_ptr = meta;
-
-        return ptr;
+        current = current->next; // Avanzar al siguiente bloque
     }
-    return NULL; 
+
+    // Ubicar inicio del nuevo bloque de memoria y solicitarlo al sistema operativo
+    void *start_ptr = sbrk(0);
+    if (sbrk(size + META_SIZE) == (void *)-1)
+    {
+        perror("Error al asignar memoria");
+        return NULL;
+    }
+
+    // Apuntador al inicio del bloque a retornar
+    void *ptr = start_ptr + META_SIZE;
+
+    // Crear metadata del bloque y almacenarla al inicio del bloque asignado
+    block_meta meta = {size, base, 0, a};
+    a++;
+    base = start_ptr;
+    *(block_meta *)start_ptr = meta;
+    return ptr;
 }
 
 void my_free(void *ptr) {
